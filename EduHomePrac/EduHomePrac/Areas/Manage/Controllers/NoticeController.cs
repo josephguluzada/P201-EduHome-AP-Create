3654434bcb180bdc40d177f2ce80925e997e5839
yusifquluzada <yquluzade98@gmail.com>
2021-12-25 11:26:24 +0400

    bacpac file added

--- EduHomePrac/EduHomePrac/Areas/Manage/Controllers/NoticeController.cs
+++ EduHomePrac/EduHomePrac/Areas/Manage/Controllers/NoticeController.cs
@@ -29,6 +29,7 @@ namespace EduHomePrac.Areas.Manage.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public IActionResult Create(Notice notice)
         {
             _context.Notices.Add(notice);
@@ -37,5 +38,31 @@ namespace EduHomePrac.Areas.Manage.Controllers
 
             return RedirectToAction("index", "notice");
         }
+
+        public IActionResult Edit(int id)
+        {
+            Notice notice = _context.Notices.FirstOrDefault(x => x.Id == id);
+            if (notice == null) return NotFound();
+
+            return View(notice);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Notice notice)
+        {
+            Notice existNotice = _context.Notices.FirstOrDefault(x => x.Id == notice.Id);
+
+            if (existNotice == null) return NotFound();
+            if (!ModelState.IsValid) return View();
+
+            existNotice.Desc = notice.Desc;
+            existNotice.Date = notice.Date;
+
+            _context.SaveChanges();
+
+            return RedirectToAction("index", "notice");
+        }
+
     }
 }
