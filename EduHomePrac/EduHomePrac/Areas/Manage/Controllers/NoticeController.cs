077418cfe47403193a56df99aadd38b22539005c
yusifquluzada <yquluzade98@gmail.com>
2021-12-25 10:26:33 +0400

    Notice Create done

---
+++ EduHomePrac/EduHomePrac/Areas/Manage/Controllers/NoticeController.cs
@@ -0,0 +1,41 @@
+﻿using EduHomePrac.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduHomePrac.Areas.Manage.Controllers
+{
+    [Area("manage")]
+    public class NoticeController : Controller
+    {
+        private readonly DataContext _context;
+
+        public NoticeController(DataContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            List<Notice> notices = _context.Notices.ToList();
+
+            return View(notices);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Notice notice)
+        {
+            _context.Notices.Add(notice);
+            _context.SaveChanges();
+
+
+            return RedirectToAction("index", "notice");
+        }
+    }
+}
---
+++ EduHomePrac/EduHomePrac/ViewModels/HomeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using EduHomePrac.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EduHomePrac.ViewModels
+{
+    public class HomeViewModel
+    {
+        public List<Course> Courses { get; set; }
+        public List<Notice> Notices { get; set; }
+    }
+}
